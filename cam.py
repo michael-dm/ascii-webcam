82458e26382e99e157a41afa7d8cd8ece26d7612
foxypiratecove37350 <108185011+foxypiratecove37350@users.noreply.github.com>
2023-06-18 16:36:10 +0200

    Amélioration du cam.py
    J'ai fait refonctionner le `Echap`  pour quitter.

--- cam.py
+++ cam.py
@@ -1,64 +1,75 @@
-#!/usr/bin/python
-
 import cv2 as cv
 import numpy as np
+import os
 import sys
 import platform
+from pynput.keyboard import *
+
+def on_press(key):
+	finish(key)
+
+def on_release(key):
+	pass
+
+listener = Listener(on_press=on_press, on_release=on_release)
+
+def finish(key):
+	if key == Key.esc:
+		listener.stop()
+		os._exit(0)
 
 def main():
 
-  vc = None
+	vc = None
 
-  if platform.system() == 'Windows':
-    vc = cv.VideoCapture(0,cv.CAP_DSHOW)
+	if platform.system() == 'Windows':
+		vc = cv.VideoCapture(0,cv.CAP_DSHOW)
 
-  elif platform.system() == 'Linux':
-    vc = cv.VideoCapture(0)
+	elif platform.system() == 'Linux':
+		vc = cv.VideoCapture(0)
 
-  if vc.isOpened():
-    rval, frame = vc.read()
-  else:
-    rval = False
+	if vc.isOpened():
+		rval, frame = vc.read()
+	else:
+		rval = False
 
-  while rval:
-  #while True:
-    rval, frame = vc.read()
-    print(toASCII(frame))
+	if rval:
+		listener.start()
 
-    key = cv.waitKey(50) # 50ms pause -> ~20fps
-    # Press echap to end
-    if key == 27:
-      break
+	while rval:
+		rval, frame = vc.read()
+		print(toASCII(frame))
+		
 
-  sys.exit()
+	sys.exit()
 
 def toASCII(frame, cols = 120, rows = 35):
 
-  frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-  height, width = frame.shape
-  cell_width = width / cols
-  cell_height = height / rows
-  if cols > width or rows > height:
-    raise ValueError('Too many cols or rows.')
-  result = ""
-  for i in range(rows):
-    for j in range(cols):
-      gray = np.mean(
-        frame[int(i * cell_height):min(int((i + 1) * cell_height), height), int(j * cell_width):min(int((j + 1) * cell_width), width)]
-      )
-      result += grayToChar(gray)
-    result += '\n'
-  return result
+	frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	height, width = frame.shape
+	cell_width = width / cols
+	cell_height = height / rows
+	if cols > width or rows > height:
+		raise ValueError('Too many cols or rows.')
+	result = ""
+	for i in range(rows):
+		for j in range(cols):
+			gray = np.mean(
+				frame[int(i * cell_height):min(int((i + 1) * cell_height), height), int(j * cell_width):min(int((j + 1) * cell_width), width)]
+			)
+			result += grayToChar(gray)
+		result += '\n'
+	return result
 
 def grayToChar(gray):
   CHAR_LIST = ' .:-=+*#%@' # Replace by " .',;:clodxkO0KXNWM" if you want more precision.
-  num_chars = len(CHAR_LIST)
-  return CHAR_LIST[
-    min(
-      int(gray * num_chars / 255),
-      num_chars - 1
-    )
-  ]
-  
+	num_chars = len(CHAR_LIST)
+	return CHAR_LIST[
+		min(
+			int(gray * num_chars / 255),
+			num_chars - 1
+		)
+	]
+	
 if __name__ == '__main__':
-    main()
+		main()
